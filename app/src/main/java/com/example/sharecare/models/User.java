edb9fe4741648ec03c496db1fb134e29b86afee6
hanan suwan <swan.omer97@hotmail.com>
2023-06-06 23:18:10 +0300

    support sql lite for signup + fix lanaguage and religion spinners

---
+++ app/src/main/java/com/example/sharecare/models/User.java
@@ -0,0 +1,111 @@
+package com.example.sharecare.models;
+public class User {
+    private int id;
+
+
+
+    private String username;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String password;
+    private int numberOfKids;
+    private String maritalStatus;
+    private String gender;
+    private String language;
+    private String religion;
+
+    // Constructor
+    public User(String username, String phoneNumber, String email, String address, String password,
+                int numberOfKids, String maritalStatus, String gender, String language, String religion) {
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+        this.numberOfKids = numberOfKids;
+        this.maritalStatus = maritalStatus;
+        this.gender = gender;
+        this.language = language;
+        this.religion = religion;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getNumberOfKids() {
+        return numberOfKids;
+    }
+
+    public void setNumberOfKids(int numberOfKids) {
+        this.numberOfKids = numberOfKids;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+}
