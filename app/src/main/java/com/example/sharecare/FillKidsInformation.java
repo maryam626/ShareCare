546614a5f4e968124b192b2942885fd1870b9319
maryam626 <maryam_khalil_7@outlook.com>
2023-06-13 22:27:19 +0300

    adding fill kids information activity

---
+++ app/src/main/java/com/example/sharecare/FillKidsInformation.java
@@ -0,0 +1,14 @@
+package com.example.sharecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillKidsInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_kids_information);
+    }
+}
