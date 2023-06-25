62a4ae587b9f3084f47f0c525bc15e6cd7b53ea1
maryam626 <maryam_khalil_7@outlook.com>
2023-06-25 21:55:31 +0300

    create kid profile layout

--- app/src/main/java/com/example/sharecare/CreateKidProfile.java
+++ app/src/main/java/com/example/sharecare/CreateKidProfile.java
@@ -3,12 +3,36 @@ package com.example.sharecare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateKidProfile extends AppCompatActivity {
+
+    private EditText nameEt;
+    private EditText ageEt1;
+    private EditText schoolNameEt;
+    private Spinner genderSpinner1;
+    private Button finishBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_kid_profile);
+
+        nameEt = (EditText) findViewById(R.id.nameEt);
+        ageEt1 = (EditText) findViewById(R.id.ageEt1);
+        schoolNameEt = (EditText) findViewById(R.id.schoolNameEt);
+        genderSpinner1 = (Spinner) findViewById(R.id.genderSpinner1);
+        finishBtn = (Button) findViewById(R.id.finishBtn);
+
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/sharecare/models/Kid.java
+++ app/src/main/java/com/example/sharecare/models/Kid.java
@@ -3,6 +3,7 @@ package com.example.sharecare.models;
 public class Kid {
 
     private int age;
+    private String name;
     private String gender;
     private Parent parent;
     private String schoolName;
