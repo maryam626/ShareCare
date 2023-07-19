35eb422a9871dcfe0ca4f1a8a3eac57bcfedc4a8
maryam626 <maryam_khalil_7@outlook.com>
2023-07-19 18:51:56 +0300

    adding group into firebase data base after the user add a group

--- app/src/main/java/com/example/sharecare/CreateGroupActivity.java
+++ app/src/main/java/com/example/sharecare/CreateGroupActivity.java
@@ -3,23 +3,37 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sharecare.Logic.GroupsDatabaseHelper;
 import com.example.sharecare.Logic.UsersDatabaseHelper;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
+    private static final String TAG = "create group activity";
+
+
     private EditText groupNameEditText, descriptionEditText,streetEditText;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
 
 
@@ -54,6 +68,7 @@ public class CreateGroupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 createGroup();
+                addingGroupToFirebase();
                 Intent intent = new Intent(CreateGroupActivity.this, MyGroupsActivity.class);
                 Bundle extras = new Bundle();
                 extras.putInt("userid", loggedInUserId);
@@ -64,6 +79,38 @@ public class CreateGroupActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void addingGroupToFirebase() {
+        Map<String, Object> group = new HashMap<>();
+        group.put("groupName",groupNameEditText.getText().toString());
+        group.put("briefInformation",descriptionEditText.getText().toString());
+        group.put("host",loggedInUsername);
+        group.put("participants",participantsSpinner.getSelectedItem().toString());
+        group.put("city",CitySpinner.getSelectedItem().toString());
+        group.put("street",streetEditText.getText().toString());
+
+
+        Task<DocumentReference> referenceTask =  db.collection("Groups")
+                .add(group)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        while(!referenceTask.isComplete()){
+
+        }
+
     }
 
     private void loadCities() {
--- app/src/main/java/com/example/sharecare/models/Group.java
+++ app/src/main/java/com/example/sharecare/models/Group.java
@@ -12,9 +12,115 @@ public class Group {
     private int capacity;
     private int ageFrom;
     private int ageTo;
+    private String city;
+    private String street;
 
+    public Group(String groupName, String briefInformation, Host host, ArrayList<Parent> participants, ArrayList<Activity> activities, int capacity, int ageFrom, int ageTo, String city, String street) {
+        this.groupName = groupName;
+        this.briefInformation = briefInformation;
+        this.host = host;
+        this.participants = participants;
+        this.activities = activities;
+        this.capacity = capacity;
+        this.ageFrom = ageFrom;
+        this.ageTo = ageTo;
+        this.city = city;
+        this.street = street;
+    }
 
+    public String getGroupName() {
+        return groupName;
+    }
 
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
 
+    public String getBriefInformation() {
+        return briefInformation;
+    }
 
+    public void setBriefInformation(String briefInformation) {
+        this.briefInformation = briefInformation;
+    }
+
+    public Host getHost() {
+        return host;
+    }
+
+    public void setHost(Host host) {
+        this.host = host;
+    }
+
+    public ArrayList<Parent> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(ArrayList<Parent> participants) {
+        this.participants = participants;
+    }
+
+    public ArrayList<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(ArrayList<Activity> activities) {
+        this.activities = activities;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getAgeFrom() {
+        return ageFrom;
+    }
+
+    public void setAgeFrom(int ageFrom) {
+        this.ageFrom = ageFrom;
+    }
+
+    public int getAgeTo() {
+        return ageTo;
+    }
+
+    public void setAgeTo(int ageTo) {
+        this.ageTo = ageTo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupName='" + groupName + '\'' +
+                ", briefInformation='" + briefInformation + '\'' +
+                ", host=" + host +
+                ", participants=" + participants +
+                ", activities=" + activities +
+                ", capacity=" + capacity +
+                ", ageFrom=" + ageFrom +
+                ", ageTo=" + ageTo +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
 }
