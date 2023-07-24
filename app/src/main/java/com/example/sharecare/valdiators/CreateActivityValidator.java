8f208179c1d2ae463fe116992821026368e53d0d
hanan suwan <swan.omer97@hotmail.com>
2023-07-24 23:01:34 +0300

    for create activity seperate validators, storing to db , and activity logic

---
+++ app/src/main/java/com/example/sharecare/valdiators/CreateActivityValidator.java
@@ -0,0 +1,38 @@
+package com.example.sharecare.valdiators;
+import android.text.TextUtils;
+
+public class CreateActivityValidator {
+
+    public static boolean isActivityNameValid(String activityName) {
+        return !TextUtils.isEmpty(activityName) && activityName.length() >= 2 && activityName.length() <= 20;
+    }
+
+    public static boolean isDateValid(String date) {
+        // You can implement date validation logic here if needed.
+        return !TextUtils.isEmpty(date);
+    }
+
+    public static boolean isTimeValid(String time) {
+        // You can implement time validation logic here if needed.
+        return !TextUtils.isEmpty(time);
+    }
+
+    public static boolean isCapacityValid(String capacity) {
+        try {
+            int capacityValue = Integer.parseInt(capacity);
+            return capacityValue >= 1 && capacityValue <= 100;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isAgeRangeValid(String ageFrom, String ageTo) {
+        try {
+            int ageFromValue = Integer.parseInt(ageFrom);
+            int ageToValue = Integer.parseInt(ageTo);
+            return ageFromValue >= 1 && ageFromValue <= 99 && ageToValue >= 1 && ageToValue <= 99 && ageFromValue < ageToValue;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
